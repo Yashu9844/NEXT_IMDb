f49fa45a8116650891fd495462ac9c4639276b13
Yashavanth R Siddesh <142719716+Yashu9844@users.noreply.github.com>
2024-08-07 17:59:33 +0530

    add handle error

---
+++ src/app/error.jsx
@@ -0,0 +1,23 @@
+"use client"
+
+import React, { useEffect } from 'react'
+
+
+const error = ({error,reset}) => {
+    useEffect(()=>{
+console.log(error)
+    },[error])
+
+  return (
+    <div className='text-center mt-10'>
+      <h1>Somethingg went Wrong</h1>
+      <button className='hover:text-amber-600' onClick={()=>reset()}>
+        Try Again
+      </button>
+    </div>
+
+
+  )
+}
+
+export default error
